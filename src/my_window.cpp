3989201cbbb7bc8e2f46047f05127d3dd6a8a359
timeopochin <timeo.sam.pochin@gmail.com>
2018-03-07 00:48:44 +0000

    Added default constructor

--- src/my_window.cpp
+++ src/my_window.cpp
@@ -1,1 +1,14 @@
 #include "my_window.h"
+
+MyWindow::MyWindow() : QWidget(), currentPoint(0)
+{
+    m_point1 = new QPoint();
+    m_point2 = new QPoint();
+    m_point3 = new QPoint();
+
+    m_painter = new QPainter(this);
+    m_pen = new QPen(Qt::black);
+
+    m_pen->setWidth(1);
+    m_painter->setPen(&m_pen);
+}
